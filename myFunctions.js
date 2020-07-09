ec03ac9b3830d23aee848513c3d3e19530128d04
Nolan Barham <nolander91b@gmail.com>
2020-07-09 14:04:11 -0700

    added history container to inde.html w/ script

--- myFunctions.js
+++ myFunctions.js
@@ -6,12 +6,18 @@ let pokeArray = [
     {name:'squirtle', image:'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png'},
 ];
 index = 0;
+histIndex = 0;
 
 let picElement = document.getElementById('pokePic');
 
 let pokeName = document.getElementById('name');
 
 let button = document.getElementById('button');
+
+let historyPic = document.getElementById('histPic');
+
+let historyName = document.getElementById('name2')
+
 button.addEventListener('click', pokeChange);
 
 function pokeChange(){
@@ -20,5 +26,10 @@ function pokeChange(){
     };
     picElement.src = pokeArray[index].image;
     pokeName.innerHTML = pokeArray[index].name;
+    historyPic.src = pokeArray[histIndex].image;
+    historyName.innerHTML = pokeArray[histIndex].name;
     index++;
+    if(index > 0){
+        histIndex = index -1;
+    }
 };
